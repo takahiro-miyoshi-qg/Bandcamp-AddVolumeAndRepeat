682df9fb7c5e043a9c3286ac00749f076c4659d1
QuineGhost <quineghost@gmail.com>
2015-08-14 16:13:19 +0900

    aa

--- scripts/volume.js
+++ scripts/volume.js
@@ -1,19 +1,40 @@
-var progressBar = document.querySelector('.progbar');
-var audioTag = document.querySelector('audio');
-var playButton = document.querySelector('.playbutton');
-var volume = document.createElement('input');
+//add volume button
+var progressBar = document.querySelector(".progbar");
+var audioTag = document.querySelector("audio");
+var playButton = document.querySelector(".playbutton");
+var volume = document.createElement("input");
 var audioSource;
 
 progressBar.appendChild(volume);
-volume.type = 'range';
-volume.min = '0.0';
-volume.max = '1.0';
-volume.step = '0.01';
-volume.value = '1.0';
+volume.type = "range";
+volume.min = "0.0";
+volume.max = "1.0";
+volume.step = "0.01";
+volume.value = "1.0";
 
-playButton.addEventListener('click', function(e){
-	audioSource = audioTag.getAttribute('src');
+playButton.addEventListener("click", function() {
+	audioSource = audioTag.getAttribute("src");
 })
-volume.addEventListener('input', function(){
+volume.addEventListener("input", function() {
 	audioTag.volume = volume.value;
 })
+
+//add repeat button
+var repeatButton = document.createElement("input");
+var repeatButtonLabel = document.createElement("span");
+progressBar.appendChild(repeatButton);
+progressBar.appendChild(repeatButtonLabel);
+repeatButton.class = "repeatButton";
+repeatButton.type = "checkbox";
+repeatButtonLabel.textContent = "repeat";
+
+repeatButton.addEventListener("click", function() {
+	if(repeatButton.checked) {
+		repeatButton.setAttribute("checked", "");
+		audioTag.setAttribute("loop", "");
+	} else {
+		repeatButton.removeAttribute("checked");
+		audioTag.removeAttribute("loop");
+	}
+})
+
