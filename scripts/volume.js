8e5f3845f01e47b17e16168f16558591288ecb6a
quineghost <quineghost@gmail.com>
2016-07-09 10:55:15 +0900

    indent change

--- scripts/volume.js
+++ scripts/volume.js
@@ -2,31 +2,31 @@ var progbar = $('.progbar');
 var audio = $('audio');
 
 var volume = $('<input>').attr({
-	id: 'volume',
-	type: 'range',
-	min: '0.0',
-	max: '1.0',
-	step: '0.01',
-	value: '1.0'
+  id: 'volume',
+  type: 'range',
+  min: '0.0',
+  max: '1.0',
+  step: '0.01',
+  value: '1.0'
 }).appendTo(progbar);
 
 var repeatButton = $('<input>').attr({
-	class: 'repeatButton',
-	type: 'checkbox'
+  class: 'repeatButton',
+  type: 'checkbox'
 }).appendTo(progbar);
 
 var repeatButtonLabel = $('<span>').text('repeat').appendTo(progbar);
 
 volume.on('input', function() {
   "use strict";
-	audio[0].volume = volume[0].value;
+  audio[0].volume = volume[0].value;
 });
 
 repeatButton.click(function() {
   "use strict";
-	if(repeatButton[0].checked) {
-		audio.attr('loop', 'loop');
-	} else {
-		audio.removeAttr('loop');
-	}
+  if(repeatButton[0].checked) {
+    audio.attr('loop', 'loop');
+  } else {
+    audio.removeAttr('loop');
+  }
 });
