b71af45465a512911d3c267f1abe1b4afb083b36
quineghost <quineghost@gmail.com>
2016-07-12 22:56:46 +0900

    to opp

--- scripts/volume.js
+++ scripts/volume.js
@@ -1,32 +1,41 @@
-var progbar = $('.progbar');
-var audio = $('audio');
+var Volume = function() {
+  this.progbar = $('.progbar');
+  this.audio = $('audio');
+  this.volume = $('<input>').attr({
+    id: 'volume',
+    type: 'range',
+    min: '0.0',
+    max: '1.0',
+    step: '0.01',
+    value: '1.0'
+  });
+  this.repeatButton = $('<input>').attr({
+    class: 'repeatButton',
+    type: 'checkbox'
+  });
+  this.repeatButtonLabel = $('<span>').text('repeat');
+};
 
-var volume = $('<input>').attr({
-  id: 'volume',
-  type: 'range',
-  min: '0.0',
-  max: '1.0',
-  step: '0.01',
-  value: '1.0'
-}).appendTo(progbar);
+Volume.prototype.main = function() {
+  this.volume.appendTo(this.progbar);
+  this.repeatButton.appendTo(this.progbar);
+  this.repeatButtonLabel.appendTo(this.progbar);
+};
 
-var repeatButton = $('<input>').attr({
-  class: 'repeatButton',
-  type: 'checkbox'
-}).appendTo(progbar);
+Volume.prototype.setEventHandler = function() {
+  var self = this;
+  self.volume.on('input', function() {
+    self.audio[0].volume = self.volume[0].value;
+  });
+  self.repeatButton.on('click', function() {
+    if(self.repeatButton[0].checked) {
+      self.audio.attr('loop', 'loop');
+    } else {
+      self.audio.removeAttr('loop');
+    }
+  });
+};
 
-var repeatButtonLabel = $('<span>').text('repeat').appendTo(progbar);
-
-volume.on('input', function() {
-  "use strict";
-  audio[0].volume = volume[0].value;
-});
-
-repeatButton.click(function() {
-  "use strict";
-  if(repeatButton[0].checked) {
-    audio.attr('loop', 'loop');
-  } else {
-    audio.removeAttr('loop');
-  }
-});
+var volume = new Volume();
+volume.main();
+volume.setEventHandler();
